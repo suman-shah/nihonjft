d8bd4ee21d439e584d16a3d09dca91547b5766b9
suman-shah <sumanshah485@gmail.com>
2024-10-23 13:23:38 +0545

    splash screen

---
+++ app/src/main/java/com/japan/nihonjft/SplashActivity.java
@@ -0,0 +1,48 @@
+package com.japan.nihonjft;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActivity extends AppCompatActivity {
+    private TextView appName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+
+        appName = findViewById(R.id.textView3);
+        Typeface typeface = ResourcesCompat.getFont(this, R.font.notosansjpregular);
+        appName.setTypeface(typeface);
+
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.myanim);
+        appName.setAnimation(animation);
+
+        new Thread(){
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+        }.start();
+    }
+}
