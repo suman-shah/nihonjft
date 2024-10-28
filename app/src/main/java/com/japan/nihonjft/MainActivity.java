8afac222e8e6d6f69ead8583f5a2fc51c15eedd3
suman-shah <sumanshah485@gmail.com>
2024-10-28 16:51:00 +0545

    day 2 time before part 3

--- app/src/main/java/com/japan/nihonjft/MainActivity.java
+++ app/src/main/java/com/japan/nihonjft/MainActivity.java
@@ -1,13 +1,16 @@
 package com.japan.nihonjft;
 
 import android.os.Bundle;
-import android.view.View;
+import android.view.MenuItem;
 import android.view.Menu;
+import android.widget.FrameLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -22,24 +25,51 @@ public class MainActivity extends AppCompatActivity {
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
     private BottomNavigationView bottomNavigationView;
-
-
+    private FrameLayout main_frame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Inflate the layout using binding
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        // Initialize BottomNavigationView and FrameLayout after setContentView()
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        main_frame = findViewById(R.id.main_frame);
+
+        // Set default fragment (home)
+        setFragment(new CategoryFragment());
+
+        // Set up the listener for BottomNavigationView
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                // Replace switch-case with if-else structure
+                if (item.getItemId() == R.id.nav_homes) {
+                    setFragment(new CategoryFragment());
+                    return true;
+                } else if (item.getItemId() == R.id.nav_leaderboard) {
+                    setFragment(new LeaderboardFragment());
+                    return true;
+                } else if (item.getItemId() == R.id.nav_account) {
+                    setFragment(new AccountFragment());
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
 
         setSupportActionBar(binding.appBarMain.toolbar);
 
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
+        // Passing each menu ID as a set of Ids because each menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,R.id.nav_leaderboard,R.id.nav_account)
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
@@ -59,5 +89,11 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.main_frame, fragment);
+        transaction.commit();
     }
 }
